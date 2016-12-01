b12e12fea9623844baf812a39dc7407bfc78fe29
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-12-01 11:21:31 +0100

    Match Windows batch features with Unix version

--- windows/xdebug.bat
+++ windows/xdebug.bat
@@ -1,9 +1,23 @@
 @echo off
-rem Update according to your needs
-SET XDEBUG_FILE=php_xdebug-2.4.0-7.0-vc14.dll
-SET XDEBUG_PATH=C:\xampp-php7\php\ext\
+setlocal enableextensions enabledelayedexpansion
+rem Defaults to be overridden in ~/.config/xdebug_wrapper.bat
 SET XDEBUG_IDEKEY=PHPSTORM
+SET PHP_BINARY=php
 
-rem No need to touch this
-SET XDEBUG_CONFIG=idekey=%XDEBUG_IDEKEY%
-php -dzend_extension="%XDEBUG_PATH%%XDEBUG_FILE%" %*
+rem Override defaults
+SET XDEBUG_OVERRIDE_FILE=%UserProfile%\.config\xdebug_wrapper.bat
+IF EXIST %XDEBUG_OVERRIDE_FILE% (
+	CALL %XDEBUG_OVERRIDE_FILE%
+)
+
+IF NOT DEFINED EXTENSION_PATH (
+	for /f %%i in ('%PHP_BINARY% -r "echo ini_get('extension_dir');"') do set EXTENSION_PATH=%%i
+)
+
+rem Find xdebug file if not set
+IF NOT DEFINED XDEBUG_FILE (
+	for /f "delims=" %%j in ('dir /b %EXTENSION_PATH% ^| %windir%\System32\find "xdebug"') do set XDEBUG_FILE=%%j
+) 
+
+rem Execute binary
+%PHP_BINARY% -dzend_extension="%EXTENSION_PATH%\%XDEBUG_FILE%" -dxdebug.idekey="%XDEBUG_IDEKEY%" %*
